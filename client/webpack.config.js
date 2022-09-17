87e1c34d241b168a162ec738efb8ae3f1c9212c8
abbygraves <abbygraves14@gmail.com>
2022-09-17 17:54:54 -0500

    changed mode to production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,7 @@ const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: "production",
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
